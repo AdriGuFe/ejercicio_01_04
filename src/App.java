90ffc62b722f0b658d6eeceb87e8cb27d19e254d
Adrian Gutierrez <capitanadriang@gmail.com>
2025-09-25 10:25:45 +0200

    added schedule

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "LUNES", "MARTES", "MIERCOLES","JUEVES","VIERNES");
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "SI", "PROG", "SI","IPE","BD");
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "SI", "PROG", "SI","IPE","BD");
+            System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "LM", "PROG", "IPE","PROG","SI");
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "LM", "BD", "DIG","PROG","ED");
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "BD", "BD", "PROG","PROG","ED");
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "BD", "SOS", "PROG","LM","ED");
+
+    
+    
+    
     }
+
+
 }
