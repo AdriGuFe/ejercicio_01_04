28b1df7d997747bdf9f81705b2d0480252c50129
Adrian Gutierrez <capitanadriang@gmail.com>
2025-09-25 12:32:38 +0200

    added colour

--- src/App.java
+++ src/App.java
@@ -1,11 +1,26 @@
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
 public class App {
+    // Códigos ANSI para colores de texto
+public static final String NEGRO = "\u001B[30m";
+public static final String ROJO = "\u001B[31m";
+public static final String VERDE = "\u001B[32m";
+public static final String AMARILLO = "\u001B[33m";
+public static final String AZUL = "\u001B[34m";
+public static final String PURPURA = "\u001B[35m";
+public static final String BLANCO = "\u001B[37m";
+public static final String CYAN = "\033[0;36m";
+public static final String RESET = "\033[0m";  // Text Reset
+    
     public static void main(String[] args) throws Exception {
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "LUNES", "MARTES", "MIERCOLES","JUEVES","VIERNES");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "SI", "PROG", "SI","IPE","BD");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "SI", "PROG", "SI","IPE","BD");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "LM", "PROG", "IPE","PROG","SI");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "LM", "BD", "DIG","PROG","ED");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "BD", "BD", "PROG","PROG","ED");
-        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "BD", "SOS", "PROG","LM","ED");
+        System.out.println(BLANCO);
+        System.out.printf("%-15s%-15s%-15s%-15s%-15s%n", "LUNES", "MARTES", "MIERCOLES","JUEVES","VIERNES");
+        System.out.println(RESET);
+        System.out.printf("%-20s%-20s%-20s%-20s%-20s%n",AMARILLO+"SI", AZUL+"PROG", AMARILLO+"SI",ROJO+"IPE",PURPURA+"BD");
+        System.out.printf("%-20s%-20s%-20s%-20s%-20s%n", AMARILLO+"SI",AZUL+"PROG",AMARILLO+"SI",ROJO+"IPE",PURPURA+"BD");
+        System.out.printf("%-20s%-20s%-20s%-20s%-20s%n", BLANCO+"LM", AZUL+"PROG", ROJO+"IPE",AZUL+"PROG",AMARILLO+"SI");
+        System.out.printf("%-20s%-20s%-22s%-20s%-20s%n", BLANCO+"LM", PURPURA+"BD", CYAN +"DIG",AZUL+"PROG",NEGRO+"ED");
+        System.out.printf("%-20s%-20s%-20s%-20s%-20s%n", PURPURA+"BD", PURPURA+"BD", AZUL+"PROG",AZUL+"PROG",NEGRO+"ED");
+        System.out.printf("%-20s%-20s%-20s%-20s%-20s%n", PURPURA+"BD", VERDE+"SOS", AZUL+"PROG",BLANCO+"LM",NEGRO+"ED");
     }
 }
