a6785b48f92e97f86008b2f3a9fa651bd9b41dbc
Adrian Gutierrez <capitanadriang@gmail.com>
2025-09-25 10:26:35 +0200

    "added schedule"

--- src/App.java
+++ src/App.java
@@ -3,15 +3,9 @@ public class App {
         System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "LUNES", "MARTES", "MIERCOLES","JUEVES","VIERNES");
         System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "SI", "PROG", "SI","IPE","BD");
         System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "SI", "PROG", "SI","IPE","BD");
-            System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "LM", "PROG", "IPE","PROG","SI");
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "LM", "PROG", "IPE","PROG","SI");
         System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "LM", "BD", "DIG","PROG","ED");
         System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "BD", "BD", "PROG","PROG","ED");
         System.out.printf("%-10s%-10s%-10s%-10s%-10s%n", "BD", "SOS", "PROG","LM","ED");
-
-    
-    
-    
     }
-
-
 }
